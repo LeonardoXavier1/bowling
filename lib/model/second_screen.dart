2558fac615c405111843a726b1419f75ab951ba9
LeonardoXavier1 <leonardomx500@gmail.com>
2023-08-24 10:37:27 -0300

    9 boxes created correctly / total score of pins knocked down correct / just missing the tenth box and the logic for strikes and etc.

--- lib/model/second_screen.dart
+++ lib/model/second_screen.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-import 'line_painter.dart';
-
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget {
   const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+class _SecondScreenState extends State<SecondScreen> {
+ List<List<int?>> numbers1 = List.generate(3, (index) => [null, null, null]);
+  List<List<int?>> numbers2 = List.generate(3, (index) => [null, null, null]);
+  List<List<int>> sums = List.generate(3, (index) => [0, 0, 0]);
+
+  int currentRow = 0;
+  int currentCol = 0;
+
+
 
   //=================================================================================================================================================
   //==========================================================      VOIDS            ================================================================
   //=================================================================================================================================================
+void fillSquare(int number) {
+    if (numbers1[currentRow][currentCol] == null) {
+      setState(() {
+        numbers1[currentRow][currentCol] = number;
+      });
+    } else if (numbers2[currentRow][currentCol] == null) {
+      setState(() {
+        numbers2[currentRow][currentCol] = number;
+        sums[currentRow][currentCol] = numbers1[currentRow][currentCol]! + numbers2[currentRow][currentCol]!;
+      });
 
+      // Move to the next cell
+      if (currentCol < 2) {
+        currentCol++;
+      } else {
+        currentCol = 0;
+        if (currentRow < 2) {
+          currentRow++;
+        } else {
+          // Check if the last square is filled
+          if (currentRow == 2 && currentCol == 2 && numbers1[2][2] != null && numbers2[2][2] != null) {
+            setState(() {
+              currentRow = 0;
+              currentCol = 0;
+            });
+          }
+        }
+      }
+    }
+  }
   //=================================================================================================================================================
   //==========================================================      /VOIDS           ================================================================
 
@@ -20,9 +60,7 @@ class SecondScreen extends StatelessWidget {
   //=================================================================================================================================================
 
 
-
-  
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 59, 3, 77),
@@ -57,87 +95,83 @@ class SecondScreen extends StatelessWidget {
                 return Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      fillSquare(index);
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15),
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
                     ),
                     child: Text(
                       '$index',
-                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
                 );
               }),
             ),
           ),
-          const SizedBox(height: 70),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              for (int row = 0; row < 3; row++)
-                Column(
-                  children: List.generate(3, (index) {
-                    final number = row * 3 + index + 1;
-                    return Container(
-                      width: 90,
-                      height: 90,
-                      alignment: Alignment.center,
+          const SizedBox(height: 20), // Espaço entre os botões e os quadrados
+          for (int i = 0; i < 3; i++) // Criar 3 linhas de quadrados
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  for (int j = 0; j < 3; j++) // Criar 3 quadrados por linha
+                    Container(
+                      width: 100,
+                      height: 100,
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.black, width: 2),
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: CustomPaint(
-                        painter: LinePainter(),
-                        child: Align(
-                          alignment: Alignment.topCenter,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 100),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0, top: 8.0),
                             child: Text(
-                              '$number',
-                              style: const TextStyle(color: Colors.green),
+                              numbers1[i][j] != null ? '${numbers1[i][j]}' : '',
+                              style: const TextStyle(color: Colors.white),
                             ),
                           ),
-                        ),
-                      ),
-                    );
-                  }),
-                ),
-            ],
-          ),
-          const SizedBox(height: 40),
-          Column(
-            children: [
-              Container(
-                width: 90,
-                height: 90,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black, width: 2),
-                ),
-                child: CustomPaint(
-                  painter: LinePainter(),
-                  child: const Align(
-                    alignment: Alignment.topCenter,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 100),
-                      child: Text(
-                        '10',
-                        style: TextStyle(color: Colors.green),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 0, right: 8.0),
+                              child: Text(
+                                numbers2[i][j] != null ? '${numbers2[i][j]}' : '',
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Score: ${sums[i][j]}',
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
         ],
       ),
     );
   }
+
 
 
   //=================================================================================================================================================
